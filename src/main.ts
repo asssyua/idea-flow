30ff8cc40704b3b0a8c813de06f91060911d0fe6
asssyua <telitsa.anna2005@gmail.com>
2025-12-02 00:09:57 +0300

    chore: add front to back

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,12 @@ async function bootstrap() {
     disableErrorMessages: false, 
   }));
   
-  app.enableCors();
+   app.enableCors({
+    origin: ['http://localhost:3001', 'http://frontend:3001'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+  });
 
   const adminSeed = app.get(AdminSeed);
   await adminSeed.seed();
